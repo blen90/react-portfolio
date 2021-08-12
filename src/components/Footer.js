43ad5473111714aed8818c8a70fa69b2bb504db2
Blenda Orellana <blen.or90@gmail.com>
2021-08-12 15:00:07 -0500

    removed space below footer, added links to technologies in resume page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ import '../pages/style.css';
 export default function Footer() {
     return (
         <div className="footer-container text-center">
-            <div className="footer text-center">
                 <a href="https://www.github.com/blen90" target="_blank" rel="noreferrer"><img src="../assets/icons/github.jpg" alt="github" className="github-logo"></img></a>
                 <a href="https://www.linkedin.com/in/blenda-orellana" target="_blank" rel="noreferrer"><img src="../assets/icons/linkedin.jpg" alt="linkedin" className="linkedin-logo"></img></a>
-            </div>
         </div>
 
     );
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,16 +6,25 @@ export default function Resume() {
         <div className="resume col-12-lg col-6-md col-3-sm text-center">
             <h1>Learned Technologies</h1>
             <div className="skill-container ">
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/html.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/css.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/js.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/node.jpg" alt="logo"></img></li>
+                <li className="skill"><a href="https://www.w3schools.com/html/default.asp" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/html.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://www.w3schools.com/css/default.asp" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/css.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://www.w3schools.com/js/default.asp" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/js.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://nodejs.org/en/docs/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/node.jpg" alt="logo"></img></a></li>
                 <br></br>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/mysql.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/mongo.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/react.jpg" alt="logo"></img></li>
-                <li className="skill"><img className="skill-logo display:inline" src="../assets/icons/graphql.jpg" alt="logo"></img></li>
+                <li className="skill"><a href="https://dev.mysql.com/doc/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/mysql.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/mongo.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/react.jpg" alt="logo"></img></a></li>
+                <li className="skill"><a href="https://graphql.org/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/graphql.jpg" alt="logo"></img></a></li>
             </div>
         </div>
     );
 }
+
+
+
+
+
+
+
+
+
