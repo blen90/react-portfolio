0ce14a99d515b826b639bd946b96f62a47e30626
Blenda Orellana <blen.or90@gmail.com>
2021-08-12 21:39:36 -0500

    styling changes on logos, added link for google drive resume, added icon to tab

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,6 +4,7 @@ export default function Resume() {
     return (
        
         <div className="resume col-12-lg col-6-md col-3-sm text-center">
+           
             <h1>Learned Technologies</h1>
             <div className="skill-container ">
                 <li className="skill"><a href="https://www.w3schools.com/html/default.asp" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/html.jpg" alt="logo"></img></a></li>
@@ -15,6 +16,8 @@ export default function Resume() {
                 <li className="skill"><a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/mongo.jpg" alt="logo"></img></a></li>
                 <li className="skill"><a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/react.jpg" alt="logo"></img></a></li>
                 <li className="skill"><a href="https://graphql.org/" target="_blank" rel="noreferrer"><img className="skill-logo display:inline" src="../assets/icons/graphql.jpg" alt="logo"></img></a></li>
+                <br></br>
+                <a href="https://drive.google.com/file/d/1DX50CG6UXmlpGEqXAGVL5MvHmI8Lzm_b/view?usp=sharing"target="_blank" rel="noreferrer">Link to Resume on Google Drive</a>
             </div>
         </div>
     );
