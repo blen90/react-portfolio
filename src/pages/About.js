47ceb2d35e9c3ae00b361e8bbc007d5a40c15f19
Blenda Orellana <blen.or90@gmail.com>
2021-09-07 14:44:34 -0500

    fixed spelling error in about me page, added correct google drive resume link

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className="card-body ">
         <h1>About Me</h1>
         <img src={process.env.PUBLIC_URL + '/assets/images/blenda1.jpg'} alt="blenda" className="blenda"></img>
-        <p>Hello! My name is Blenda, I'm originially from Guatemala, but I'm currently living in Austin, Texas.</p>
+        <p>Hello! My name is Blenda, I'm originally from Guatemala, but I'm currently living in Austin, Texas.</p>
         <p> I enjoy going on long walks and hikes with my two dogs, cooking all sorts of international foods and travelling.</p>
         <p>I just received my certificate for Full Stack Web Development Certificate (Coding Boot Camp) from the University of Minnesota. This was an intensive 12-week accredited Bootcamp with new course material introduced every 2 or 3 days. </p>
         <p>This Bootcamp required an individual weekly effort ranging between 80-100+ hours in order to attend classes, complete real-world programming assignments, and collaborate on 3 small group projects with other students who attended the Bootcamp.</p>
